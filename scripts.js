bac59cd91b4d16eb3f845cfd3a523923b2ff37bb
Gustav4GS <bongerdm@gmail.com>
2019-12-20 13:20:40 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,84 +1,83 @@
-/// получаем массив кнопок 
-
-var monthbtns = document.getElementsByClassName("month");
-var monthbtnstoArr = Array.from(monthbtns);
-
 /// массив названий кнопок
 
-const months = ["jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"];
+const months = [
+  "jan",
+  "feb",
+  "mar",
+  "apr",
+  "may",
+  "jun",
+  "jul",
+  "aug",
+  "sep",
+  "oct",
+  "nov",
+  "dec"
+];
 
-/// назначем тексту кнопке значение соответствующего месяца
+/// формируем кнопки месяцев и назначем кнопке текст
 
-months.forEach(function(month, index){
-    monthbtns.item(index).textContent = month;
+for (i = 0; i < months.length; i++) {
+  $(".month_buttons").append('<button class="act">' + months[i] + "</button>");
+}
+
+/// получаем массив кнопок в качестве объектов
+
+var monthbtns = document.getElementsByClassName("act");
+var monthbtnstoArr = Array.from(monthbtns);
+
+months.forEach(function(month, index) {
+  monthbtns.item(index).textContent = month;
 });
 
 /// присоединяем listener к кнопкам , который при нажатии на кнопку с опреедленным месяцем :  очищает окно активностей, подгружает акивности  ,  передает кнопку  в функцию loadactivitytodocument()
 
-monthbtnstoArr.forEach(function(monthbtn){
-    monthbtn.addEventListener("click", function(){
+monthbtnstoArr.forEach(function(monthbtn) {
+  monthbtn.addEventListener("click", function() {
     console.log("clicked me");
     clearactivities();
     loadactivitytodocument(monthbtn);
-
-    });
+  });
 });
-
-/// ----
-
 
 ///----   получить  JSON как JS  Объект   (сделано с помощью JQuery т.к. было удобно)
 
 var items = null;
 var items2 = [];
 
-$.getJSON("https://dl.dropboxusercontent.com/s/rvz2q36x05wt8gz/calendardata.json?dl=0", function(data) {
-  items = data;
-  items.forEach(function(item, index) {
+$.getJSON(
+  "https://dl.dropboxusercontent.com/s/rvz2q36x05wt8gz/calendardata.json?dl=0",
+  function(data) {
+    items = data;
+    items.forEach(function(item, index) {
+      console.log(item);
 
-console.log(item);
-
-    items2.push(item);
-  });
- 
-});
-
-/// ----
-
-
+      items2.push(item);
+    });
+  }
+);
 
 /// загружает данные на страничку в список  с id list
 
-function loadactivitytodocument(monthbtn){    // если значение поля месяц совпадает с именем кнопки то перебираем любой массив из активностей, и присоединяем к списку новые строки li
- 
- items2.forEach(function (item){
+function loadactivitytodocument(monthbtn) {
+  // если значение поля месяц совпадает с именем кнопки то перебираем любой массив из активностей, и присоединяем к списку новые строки li
 
-   var space = " ";
+  items2.forEach(function(item) {
+    var space = " ";
 
-    if(item["month"] === monthbtn.textContent)       
-     {
-       for(var i=0; i<item["activities"].length; i++){
+    if (item["month"] === monthbtn.textContent) {
+      for (var i = 0; i < item["activities"].length; i++) {
+        var sumactivity =
+          item.dates[i] + space + item.times[i] + space + item.activities[i];
 
-       var sumactivity = item.dates[i] + space +  item.times[i] + space + item.activities[i];
-         
-          $('#list').append('<li class="act">' + sumactivity  +  '</li>');
-       }
+        $("#list").append('<li class="activity">' + sumactivity + "</li>");
+      }
     }
- });
+  });
 }
 
-///---
+/// мето очищающий лист активностей (при нажатии кнопки вызываем)
 
-
-/// убирает активности из документа.
-
-function clearactivities(){
- $('.act').remove(); 
+function clearactivities() {
+  $(".activity").remove();
 }
-
-
-alert("script connected");
-    
-
-
-
